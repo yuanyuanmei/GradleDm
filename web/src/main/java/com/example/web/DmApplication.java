f1f5a4b0b3d9b9b1c3c3034e3e23eb4b7fd64726
yuanyuanmei <46510949+yuanyuanmei@users.noreply.github.com>
2019-06-13 17:44:26 +0800

    Update DmApplication.java
    uuu

--- web/src/main/java/com/example/web/DmApplication.java
+++ web/src/main/java/com/example/web/DmApplication.java
@@ -3,11 +3,11 @@ package com.example.web;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.example")
 @MapperScan("com.example.*.dao")
-@Component("com.example")
 public class DmApplication {
 
     public static void main(String[] args) {
