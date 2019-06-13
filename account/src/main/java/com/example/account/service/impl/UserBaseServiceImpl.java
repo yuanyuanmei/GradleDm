d9eb0f8882f1b2bb99a584a148add0aecd43f2cc
yuanyuanmei <46510949+yuanyuanmei@users.noreply.github.com>
2019-06-13 17:33:55 +0800

    Update UserBaseServiceImpl.java
    uuu

--- account/src/main/java/com/example/account/service/impl/UserBaseServiceImpl.java
+++ account/src/main/java/com/example/account/service/impl/UserBaseServiceImpl.java
@@ -1,11 +1,14 @@
 package com.example.account.service.impl;
 
+import com.alibaba.fastjson.JSONObject;
 import com.example.account.beans.UserBaseBean;
 import com.example.account.dao.UserBaseDao;
 import com.example.account.service.UserBaseService;
 import com.example.common.base.BaseServiceImpl;
+import com.example.common.util.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * 用户基础业务实现类
@@ -15,5 +18,13 @@ public class UserBaseServiceImpl extends BaseServiceImpl<UserBaseBean> implement
 
     @Autowired
     private UserBaseDao baseDao;
+
+    @Transactional
+    @Override
+    public JSONObject insert(UserBaseBean bean){
+        //生成7位随机数用户名
+        bean.setUsername(StringUtils.getItemID(7));
+        return StringUtils.formatSuccessJson("新增成功！",baseDao.insert(bean));
+    }
 
 }
