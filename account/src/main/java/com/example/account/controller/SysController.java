76ab2131b1e5dd3ad3694446d39b8b0b04cba8bf
yuanyuanmei <46510949+yuanyuanmei@users.noreply.github.com>
2019-06-13 17:30:45 +0800

    Update SysController.java
    uuu

--- account/src/main/java/com/example/account/controller/SysController.java
+++ account/src/main/java/com/example/account/controller/SysController.java
@@ -1,26 +1,25 @@
 package com.example.account.controller;
 
-import com.alibaba.fastjson.JSONObject;
 import com.example.account.beans.SysPermissionBean;
 import com.example.account.beans.SysRoleBean;
 import com.example.account.beans.UserBaseBean;
 import com.example.account.service.SysPermissionService;
 import com.example.account.service.SysRoleService;
 import com.example.account.service.UserBaseService;
+import com.example.common.annotations.CrudCustom;
+import com.example.common.annotations.ValidateCustom;
 import com.example.common.constants.ApiFuncConsts;
 import com.example.common.constants.ApiModuleConsts;
-import com.example.common.util.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
 
 
 /**
  * 权限列表管理
  */
-@Controller
+@RestController
 @RequestMapping(ApiModuleConsts.SYS)
 public class SysController {
 
@@ -33,24 +32,48 @@ public class SysController {
     @Autowired
     private SysPermissionService permsService;
 
-    //用户修改或新增
-    @ResponseBody
+    //用户列表
+    @CrudCustom(UserBaseService.class)
+    @GetMapping(ApiFuncConsts.USER)
+    public Object getUser(HttpServletRequest request){
+        return request.getAttribute("jsonObject");
+    }
+
+    //角色列表
+    @CrudCustom(UserBaseService.class)
+    @GetMapping(ApiFuncConsts.ROLE)
+    public Object getRole(HttpServletRequest request){
+        return request.getAttribute("jsonObject");
+    }
+
+    //权限列表
+    @CrudCustom(UserBaseService.class)
+    @GetMapping(ApiFuncConsts.MENU)
+    public Object getMenu(HttpServletRequest request){
+        return request.getAttribute("jsonObject");
+    }
+
+    //用户新增或修改
+    @ValidateCustom(UserBaseBean.class)
+    @CrudCustom(UserBaseService.class)
     @PostMapping(ApiFuncConsts.USER)
-    public JSONObject saveUser(UserBaseBean paramBean){
-        return StringUtils.formatSuccessJson("更新成功");
+    public Object saveUser(UserBaseBean paramBean,HttpServletRequest request){
+        return request.getAttribute("jsonObject");
     }
 
-    //用户修改或新增
-    @ResponseBody
+    //权限修改或新增
+    @ValidateCustom(SysRoleBean.class)
+    @CrudCustom(SysRoleService.class)
     @PostMapping(ApiFuncConsts.ROLE)
-    public JSONObject saveRole(SysRoleBean paramBean){
-        return StringUtils.formatSuccessJson("更新成功");
+    public Object saveRole(SysRoleBean paramBean,HttpServletRequest request){
+        return request.getAttribute("jsonObject");
     }
 
-    //用户修改或新增
-    @ResponseBody
+    //菜单修改或新增
+    @ValidateCustom(SysPermissionBean.class)
+    @CrudCustom(SysPermissionService.class)
     @PostMapping(ApiFuncConsts.MENU)
-    public JSONObject saveMenu(SysPermissionBean paramBean){
-        return StringUtils.formatSuccessJson("更新成功");
+    public Object saveMenu(SysPermissionBean paramBean,HttpServletRequest request){
+        return request.getAttribute("jsonObject");
     }
 }
